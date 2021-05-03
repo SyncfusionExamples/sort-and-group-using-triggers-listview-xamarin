7e46ba6178557ae5bbabb90766ea58bb3405350e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-05-03 15:28:56 +0530

    XAMARIN-34650 - Sample and screenshot added

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,51 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        Button SortButton;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            SortButton = bindable.FindByName<Button>("sortButton");
+            SortButton.Clicked += SortButton_Clicked;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            SortButton.Clicked -= SortButton_Clicked;
+            SortButton = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+        private void SortButton_Clicked(object sender, EventArgs e)
+        {
+            var viewModel = SortButton.BindingContext as ContactsViewModel;
+
+            if (viewModel.SortDirection == ListSortDirection.Ascending)
+                viewModel.SortDirection = ListSortDirection.Descending;
+            else
+                viewModel.SortDirection = ListSortDirection.Ascending;
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/Contacts.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Contacts : INotifyPropertyChanged
+    {
+        #region Fields
+        private string contactName;
+        private string contactNumber;
+        private ImageSource image;
+        private string contactType;
+        #endregion
+
+        #region Constructor
+        public Contacts(string name, string number)
+        {
+            contactName = name;
+            contactNumber = number;
+        }
+
+        public Contacts()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set
+            {
+                if (contactNumber != value)
+                {
+                    contactNumber = value;
+                    this.RaisedOnPropertyChanged("ContactNumber");
+                }
+            }
+        }
+
+        public ImageSource ContactImage
+        {
+            get { return this.image; }
+            set
+            {
+                this.image = value;
+                this.RaisedOnPropertyChanged("ContactImage");
+            }
+        }
+        
+        public string ContactType
+        {
+            get { return this.contactType; }
+            set
+            {
+                this.contactType = value;
+                this.RaisedOnPropertyChanged("ContactType");
+            }
+        }
+        #endregion
+
+        #region Interface Member
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+        #endregion
+    }
+}
